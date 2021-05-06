a6f8f0aefb0bfcaa57364af84686b4dca1abcbe1
zaineb KHARFATI <60789455+zaina34@users.noreply.github.com>
2021-05-06 02:28:35 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,84 @@
+<footer class="footer_section clearfix">
+			<div class="footer_widget_area sec_ptb_100 clearfix">
+				<div class="container">
+					<div class="row justify-content-lg-between">
+						<div class="col-lg-4 col-md-4 col-sm-12 col-sm-12">
+							<div class="footer_about" data-aos="fade-up" data-aos-delay="100">
+								<div class="brand_logo mb_30">
+									<a href="index.html">
+										<img src="assets/images/logo/logo_02_1x.png" srcset="assets/images/logo/logo_02_1x.png 2x" alt="logo_not_found">
+									</a>
+								</div>
+								<p class="mb_15">
+								<p> Avec 2,8 visiteurs étrangers par seconde, la France est, et reste, la première destination touristique mondiale. 
+Mais pourquoi la France attire-t-elle autant ?
+Est-ce pour Ses villes branchées ? Sa culture ? ses transports  Ou ses sublimes paysages ? 
+Peut-être est-ce un tout… Petit tour d’horizon de tout ce qui fait que nous aimons notre pays, 
+comme c’est le cas des habitants de la planète entière ! </p>								</p>
+								<div class="footer_useful_links mb_30">
+									<ul class="ul_li_block clearfix">
+										<li><a href="#!"><i class="fal fa-angle-right"></i> Rental Information</a></li>
+										<li><a href="#!"><i class="fal fa-angle-right"></i> F.A.Q.</a></li>
+									</ul>
+								</div>
+								<div class="form_item mb-0">
+									<form action="#">
+										<input id="footer_search" type="search" name="search" placeholder="Search">
+										<label for="footer_search" class="input_icon"><i class="fal fa-search"></i></label>
+									</form>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-lg-3 col-md-4 col-sm-12 col-sm-12">
+							<div class="footer_contact_info" data-aos="fade-up" data-aos-delay="200">
+								<h3 class="footer_widget_title">Contacter nous:</h3>
+								<ul class="ul_li_block clearfix">
+									<li>
+										<strong><i class="fas fa-map-marker-alt"></i> Addresse:</strong>
+										<p class="mb-0">
+										Université Paul-Valéry Montpellier 3 Route de Mende 34199 Montpellier Cedex 5
+										</p>
+									</li>
+									<li><i class="fas fa-clock"></i> 8:00-16:30</li>
+									<li><i class="far fa-angle-right"></i> Office principal</li>
+									<li><i class="fas fa-envelope"></i> <strong>garages-regions@gmail.com</strong></li>
+									<li><i class="fas fa-phone"></i> <strong>+33000000000</strong></li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="col-lg-4 col-md-4 col-sm-12 col-sm-12">
+							<div class="footer_useful_links" data-aos="fade-up" data-aos-delay="300">
+								<h3 class="footer_widget_title">Information:</h3>
+								<ul class="ul_li_block clearfix">
+									<li><a href="#!"><i class="fal fa-angle-right"></i> Lister les gares</a></li>
+									<li><a href="#!"><i class="fal fa-angle-right"></i> F.A.Q.</a></li>
+									<li><a href="#!"><i class="fal fa-angle-right"></i> A propos</a></li>
+									<li><a href="#!"><i class="fal fa-angle-right"></i> Contacter nous</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="footer_bottom text-white clearfix" data-bg-color="#000C21">
+				<div class="container">
+					<div class="row align-items-center justify-content-lg-between">
+						<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+							<p class="copyright_text mb-0">Copyright © 2021. </p>
+						</div>
+
+						<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+							<ul class="primary_social_links ul_li_right clearfix">
+								<li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#!"><i class="fab fa-instagram"></i></a></li>
+								<li><a href="#!"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#!"><i class="fab fa-youtube"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
