1c8f819e79d716d1dffc08856fb9e83fa41da2d5
zaineb KHARFATI <60789455+zaina34@users.noreply.github.com>
2021-05-06 02:29:13 +0200

    Add files via upload

---
+++ connecter.php
@@ -0,0 +1,50 @@
+
+
+ 
+ 
+ <!DOCTYPE HTML>
+  <HTML>
+  <head>
+  <TITLE>page connecter</TITLE>
+  </head>
+  <?php
+  if($_POST['email']== "" or $_POST['mdp']== "" ){
+    echo'<meta http-equiv="refresh" content="0; url=connexion.php">';
+	
+	
+  }
+  
+  
+
+  else{
+	  require_once('Tools.php');
+    $bdd =  getBD();
+		$query = "select * from utilisateur where mail='".$_POST ['email']."' && mdp='".$_POST['mdp']."'";
+
+    $rep = $bdd->query($query);
+
+    while ($ligne = $rep->fetch()) {
+      $_SESSION['utilisateur']=array(
+        'nom'=>$ligne['nom'],
+        'prenom'=>$ligne['prenom'],
+        'adresse'=>$ligne['adresse'],
+        'num'=>$ligne['num'],
+        'mail'=>$ligne['mail'],
+        'classe'=>$ligne['classe']
+      );
+    
+      echo '<meta http-equiv="Refresh" content="0;url=profil.php">';
+	
+    }
+  }
+  ?>
+ 
+  
+  
+  <body>
+   <p>Addresse ou mot de passe intouvables  ! </p>
+  <p>Veuillez ressaisir les bonnes informations svp </p>
+<a href="connexion.php">Se connecter </a>
+
+    </body>
+ </HTML>
