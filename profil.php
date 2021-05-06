c6cbc9421cacbc62a0a337d9586812c7e7123f4e
zaineb KHARFATI <60789455+zaina34@users.noreply.github.com>
2021-05-06 02:37:21 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -10,7 +10,7 @@ include("Tools.php");
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 
-		<title>Home V.1 - Rotors - Car Rental HTML5 Template</title>
+		<title>Profil </title>
 		<link rel="shortcut icon" href="assets/images/logo/favourite_icon.png">
 
 		<!-- fraimwork - css include -->
